59107e128531ef6b73866f0d0970a1734f085779
Vasily Vasinov <des.elyon@gmail.com>
2014-03-06 12:46:55 -0700

    removing rails

--- test1/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Test1::Application.config.secret_token = 'b79b6a07af3b18773799aca29b26dc1399713e3edc9d86f783703cc12edcb1130957b38282d0e67ac36164612abf8d0324317c61fd27ab5a21a951c6342c7a94'
